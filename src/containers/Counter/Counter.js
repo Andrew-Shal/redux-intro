727a91241a4a019b58ab428986d5ba36a76757cc
Andrew-Shal <shalandrew97@gmail.com>
2020-04-25 01:23:52 -0600

    added redux advance

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,46 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
-
-import {connect} from 'react-redux';
-import * as actionTypes from '../../store/actions';
+import * as actionCreators from '../../store/actions';
 
 class Counter extends Component {
-    state = {
-        counter: 0
-    }
-
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
-            case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
-                break;
-            case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
-                break;
-            case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
-                break;
-            case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
-                break;
-        }
-    }
-
     render () {
         return (
             <div>
                 <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
-                <CounterControl label="Add 5" clicked={this.props.onAddCounter}  />
-                <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter}  />
-                <hr/>
-                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <CounterControl label="Add 10" clicked={this.props.onAddCounter}  />
+                <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter}  />
+                <hr />
+                <button onClick={() => this.props.onStoreResult(this.props.ctr)}>Store Result</button>
                 <ul>
-                    {this.props.storedResults.map(storedResult => (
-                        <li key={storedResult.id} onClick={() => this.props.onDeleteResult(storedResult.id)}>{storedResult.value}</li>
+                    {this.props.storedResults.map(strResult => (
+                        <li key={strResult.id} onClick={() => this.props.onDeleteResult(strResult.id)}>{strResult.value}</li>
                     ))}
                 </ul>
             </div>
@@ -50,20 +28,21 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-        ctr:state.counter,
-        storedResults:state.results
-    };
-}
+
+        ctr: state.ctr.counter,
+        storedResults: state.res.results
+    }
+};
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch({type:actionTypes.INCREMENT}),
-        onDecrementCounter: () => dispatch({type:actionTypes.DECREMENT}),
-        onAddCounter: () => dispatch({type:actionTypes.ADD,value:5}),
-        onSubtractCounter: () => dispatch({type:actionTypes.SUBTRACT,value:5}),
-        onStoreResult: () => dispatch({type:actionTypes.STORE_RESULT}),
-        onDeleteResult: (resElId) => dispatch({type:actionTypes.DELETE_RESULT,resElId})
-    };
-}
+        onIncrementCounter: () => dispatch(actionCreators.increment()),
+        onDecrementCounter: () => dispatch(actionCreators.decrement()),
+        onAddCounter: () => dispatch(actionCreators.add(10)),
+        onSubtractCounter: () => dispatch(actionCreators.subtract(15)),
+        onStoreResult: (result) => dispatch(actionCreators.storeResult(result)),
+        onDeleteResult: (id) => dispatch(actionCreators.deleteResult(id))
+    }
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
---
+++ src/store/utility.js
@@ -0,0 +1,6 @@
+export const updateObj = (oldObj,updatedVals) => {
+    return {
+        ...oldObj,
+        ...updatedVals
+    }
+};
